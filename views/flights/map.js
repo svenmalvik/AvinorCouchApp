d2bba7a4750aafcfcbd8e2eaac53afd74e38705e
Sven Malvik <sven@malvik.de>
2011-03-08 16:49:22 +0100

    .

--- views/flights/map.js
+++ views/flights/map.js
@@ -1,21 +1,32 @@
 function(doc) {
-	if (doc.belt == null) {
-		doc.belt = "";
-	}
-	if (doc.status == null) {
-		doc.status = "";
-	}
-	if (doc.delayed == null) {
-		doc.delayed = "";
-	}
+	var date = new Date(parseInt(doc.schedule_time) + 4*3600000);	
+	var time = date.getHours() + ":" + date.getMinutes() + ":" + date.getDate();
+	var date2 = new Date();		
+	var time2 = date2.getHours() + ":" + date2.getMinutes() + ":" + date2.getDate();
 	
-	emit(	[doc.schedule_time, doc.airport], 
-		[	doc.flight_id, 
-			doc.airline, 
-			doc.belt,
-			doc.airport,
-			doc.status,
-			new Date(parseInt(doc.schedule_time))
-		]);
+	if ((date.getHours()-date2.getHours()) < 1) {	
+		if (doc.belt == null) {
+			doc.belt = "";
+		}
+		if (doc.status == null) {
+			doc.status = "";
+		}
+		if (doc.delayed == null) {
+			doc.delayed = "";
+		}
+	
+
+		emit(	[doc.schedule_time, doc.airport], 
+			[	doc.flight_id, 
+				//doc.airline, 
+				//doc.belt,
+				time,
+				time2,
+				//new Date(),
+				doc.airport,
+				doc.status
+			]
+		);
+	}	
 }
 
